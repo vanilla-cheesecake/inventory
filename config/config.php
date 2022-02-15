535e943a49c3ff7d4b26e60a40ebf6024dfd83f8
golez <golez.sf@gmail.com>
2022-02-15 11:01:33 +0800

    remote sql
    testremote sql

--- config/config.php
+++ config/config.php
@@ -1,8 +1,15 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "inventory");
+// FOR LOCALHOST SQL
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "inventory");
+
+// FOR REMOTE FREE SQL TEST-BRANCH 
+define("DB_HOST", "sql5.freesqldatabase.com");
+define("DB_USER", "sql5472887");
+define("DB_PASS", "rsSGDE2NQ3");
+define("DB_NAME", "sql5472887");
 
 
 ?>
